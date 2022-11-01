be472c736866783432c39be979f4910d34877b46
Claudio Cavalhieri Filho <cavalhieri@gmail.com>
2022-11-01 16:09:49 -0300

    code format

--- views/js/main.js
+++ views/js/main.js
@@ -15,18 +15,18 @@ fetch('memory.json')
     }
 );
 
-function create_macro (data) {    
+function create_macro(data) {    
     const macro = new Macro(data['properties']),
           fields_map = {};
 
     create_card(macro, data['root'], fields_map);
     macro.initVisibility(fields_map);
 }
-function create_card (macro, props, fields_map, output = null) {
+function create_card(macro, props, fields_map, output = null) {
     let card = macro.createCard(props['position'], props['properties'], output);
     create_field(card, props['fields'], fields_map);
 }
-function create_field (card, fields, fields_map) {
+function create_field(card, fields, fields_map) {
     for (const field of fields) {
         const new_field = card.newField(field['properties']);
         fields_map[new_field.getProps('id')] = new_field;

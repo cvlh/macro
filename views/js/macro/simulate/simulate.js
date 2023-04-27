5f3c2234b26ffb028c180ab9c452bc30f0b98edf
Claudio Cavalhieri Filho <cavalhieri@gmail.com>
2023-04-27 16:48:58 -0300

    photo video play fix

--- views/js/macro/simulate/simulate.js
+++ views/js/macro/simulate/simulate.js
@@ -126,7 +126,7 @@ export default function Simulate(__run_env = _RUN_ENVIRONMENT_.WEB) {
                             const video = addElement(this, 'video', 'item-drawing');
                             video.width = this.offsetWidth;
                             video.height = this.offsetHeight;
-                            video.setAttribute('muted', true);
+                            video.setAttribute('muted', '');
 
                             const canvas = addElement(this, 'canvas', 'item-drawing');
                             canvas.width = this.offsetWidth;

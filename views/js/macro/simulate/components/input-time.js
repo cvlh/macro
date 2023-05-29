05bca32048d798a560070221cfc03e24d0db0931
Claudio Cavalhieri Filho <cavalhieri@gmail.com>
2023-05-29 15:42:23 -0300

    update input-date

--- views/js/macro/simulate/components/input-time.js
+++ views/js/macro/simulate/components/input-time.js
@@ -5,25 +5,25 @@ import { _I18N_ } from '../../../i18n/pt-br.js';
 import { addElement } from '../../../utils/functions.js';
 
 export default function InputTime(__append, __properties) {
-    
-    if (!new.target) 
+
+    if (!new.target)
         throw new Error('InputTime() must be called with new');
 
     // VARIABLES ///////////////////////////////////////////////////////////////
-    let today, selectedDate, currentDate, currentYear, currentMonth;
-    
+    let startY, moveY, itemHeight, block = false;
+
     // CONSTANTS ///////////////////////////////////////////////////////////////
     const
         DOMElement = {
             timepicker: null
         },
 
-    _pad = (number) => number < 10 ? '0' + number : number,
-    _time = (date) => _pad(date.getDate()) + '/' + _pad((date.getMonth() + 1)) + '/' + _pad(date.getFullYear()),
+        _pad = (number) => number < 10 ? '0' + number : number,
+        _time = (date) => _pad(date.getDate()) + '/' + _pad((date.getMonth() + 1)) + '/' + _pad(date.getFullYear()),
 
-    _render = () => {      
+        _render = () => {
 
-    };
+        };
 
     // PUBLIC  /////////////////////////////////////////////////////////////////
     this.add = (keyCode) => {
@@ -32,36 +32,131 @@ export default function InputTime(__append, __properties) {
     this.clear = () => _clear();
 
     // CONSTRUCTOR /////////////////////////////////////////////////////////////
-    (function() {
+    (function () {
         DOMElement.timepicker = addElement(__append, 'div', 'picker');
 
-        DOMElement.timepicker.innerHTML = "\
-            <div class='picker-cell'> \
-                <div class='picker-control picker-control-prev'></div> \
-                <div class='picker-body'> \
-                    <div class='picker-item'>05</div> \
-                    <div class='picker-item'>06</div> \
-                    <div class='picker-item'>07</div> \
-                    <div class='picker-picked'>08</div> \
-                    <div class='picker-item'>09</div> \
-                    <div class='picker-item'>10</div> \
-                    <div class='picker-item'>11</div> \
-                </div> \
-                <div class='picker-control picker-control-next'></div> \
-            </div> \
-            <div class='picker-cell'> \
-                <div class='picker-control picker-control-prev'></div> \
-                <div class='picker-body'> \
-                    <div class='picker-item'>05</div> \
-                    <div class='picker-item'>06</div> \
-                    <div class='picker-item'>07</div> \
-                    <div class='picker-picked'>08</div> \
-                    <div class='picker-item'>09</div> \
-                    <div class='picker-item'>10</div> \
-                    <div class='picker-item'>11</div> \
-                </div> \
-                <div class='picker-control picker-control-next' data-picker-action='next'></div> \
-            </div>";
+        const hour = addElement(DOMElement.timepicker, 'div', 'picker-cell');
+        hour.addEventListener('touchstart', (evnt) => {
+            evnt.preventDefault();
+            const target = evnt.target;
+            const list = target.querySelector('.picker-list');
+            if (list) {
+                list.style.removeProperty('transition');
+                itemHeight = list.firstElementChild.offsetHeight;
+                startY = evnt.changedTouches ? evnt.changedTouches[0].pageY : evnt.pageY;
+                moveY = 0;
+            }
+
+        }, { capture: false });
+        hour.addEventListener('touchmove', (evnt) => {
+            evnt.preventDefault();
+            const target = evnt.target;
+            const list = target.querySelector('.picker-list');
+            if (list) {
+                const end_y = evnt.changedTouches ? evnt.changedTouches[0].pageY : evnt.pageY;
+                let move_y = moveY + (end_y - startY);
+
+                let up = move_y > 0 ? false : true;
+                const mov = Math.abs(move_y);
+                if (mov > itemHeight) {
+                    let highlight = list.querySelector('.picker-picked');
+                    const first = list.firstElementChild;
+                    if (up) {
+                        const removed_child = list.removeChild(first);
+                        list.appendChild(removed_child);
+
+                        highlight.className  = 'picker-item';
+                        highlight = highlight.nextSibling;
+                        highlight.className  = 'picker-picked';
+
+                        move_y += itemHeight;
+                    } else {
+                        const last = list.lastElementChild;
+                        const removed_child = list.removeChild(last);
+                        list.insertBefore(removed_child, first);
+
+                        highlight.className  = 'picker-item';
+                        highlight = highlight.previousSibling;
+                        highlight.className  = 'picker-picked';
+
+                        move_y -= itemHeight;
+                    }
+                }
+
+                startY = end_y;
+                moveY = move_y ;
+
+                list.style.top = `${moveY}px`;
+
+            }
+
+        }, { capture: false });
+        hour.addEventListener('touchend', (evnt) => {
+            evnt.preventDefault();
+            const target = evnt.target;
+            const list = target.querySelector('.picker-list');
+            if (list) {
+                list.style.transition = '0.15s top ease';
+                list.style.top = '0px';
+            }
+        }, { capture: false });
+
+        const hour_control_prev = addElement(hour, 'div', 'picker-control picker-control-prev');
+        const hour_body = addElement(hour, 'div', 'picker-body');
+        const hour_list = addElement(hour_body, 'div', 'picker-list');
+        addElement(hour_list, 'div', 'picker-item', '05');
+        addElement(hour_list, 'div', 'picker-item', '06');
+        addElement(hour_list, 'div', 'picker-item', '07');
+        addElement(hour_list, 'div', 'picker-picked', '08');
+        addElement(hour_list, 'div', 'picker-item', '09');
+        addElement(hour_list, 'div', 'picker-item', '10');
+        addElement(hour_list, 'div', 'picker-item', '11');
+        const hour_control_next = addElement(hour, 'div', 'picker-control picker-control-next');
+
+        const minute = addElement(DOMElement.timepicker, 'div', 'picker-cell');
+        const minute_control_prev = addElement(minute, 'div', 'picker-control picker-control-prev');
+        const minute_body = addElement(minute, 'div', 'picker-body');
+        const minute_list = addElement(minute_body, 'div', 'picker-list');
+        addElement(minute_list, 'div', 'picker-item', '05');
+        addElement(minute_list, 'div', 'picker-item', '06');
+        addElement(minute_list, 'div', 'picker-item', '07');
+        addElement(minute_list, 'div', 'picker-picked', '08');
+        addElement(minute_list, 'div', 'picker-item', '09');
+        addElement(minute_list, 'div', 'picker-item', '10');
+        addElement(minute_list, 'div', 'picker-item', '11');
+        const minute_control_next = addElement(minute, 'div', 'picker-control picker-control-next');
+
+        // DOMElement.timepicker.innerHTML = "\
+        //     <div class='picker-cell'> \
+        //         <div class='picker-control picker-control-prev'></div> \
+        //         <div class='picker-body'> \
+        //             <div class='picker-list'> \
+        //                 <div class='picker-item'>05</div> \
+        //                 <div class='picker-item'>06</div> \
+        //                 <div class='picker-item'>07</div> \
+        //                 <div class='picker-picked'>08</div> \
+        //                 <div class='picker-item'>09</div> \
+        //                 <div class='picker-item'>10</div> \
+        //                 <div class='picker-item'>11</div> \
+        //             </div> \
+        //         </div> \
+        //         <div class='picker-control picker-control-next'></div> \
+        //     </div> \
+        //     <div class='picker-cell'> \
+        //         <div class='picker-control picker-control-prev'></div> \
+        //         <div class='picker-body'> \
+        //             <div class='picker-list'> \
+        //                 <div class='picker-item'>05</div> \
+        //                 <div class='picker-item'>06</div> \
+        //                 <div class='picker-item'>07</div> \
+        //                 <div class='picker-picked'>08</div> \
+        //                 <div class='picker-item'>09</div> \
+        //                 <div class='picker-item'>10</div> \
+        //                 <div class='picker-item'>11</div> \
+        //             </div> \
+        //         </div> \
+        //         <div class='picker-control picker-control-next' data-picker-action='next'></div> \
+        //     </div>";
     })();
 }
 
